3daf44ca5af45e9bb714209fdea005daf916aa59
Dennis Sheets <dennissheets@outlook.com>
2022-01-27 23:18:09 -0600

    Moved all files over, updated using statements, fixed Home Index, Successful Migration

---
+++ VaishaliLiftoff/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VaishaliLiftoff.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ VaishaliLiftoff/ViewModels/StudentDetailViewModel.cs
@@ -0,0 +1,30 @@
+﻿using InstituteAdministration.Models;
+using System.Collections.Generic;
+
+namespace InstituteAdministration.ViewModels
+{
+    public class StudentDetailViewModel
+    {
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+        public string TeacherName { get; set; }
+        public string CourseText { get; set; }
+
+        public StudentDetailViewModel(Student theStudent, List<StudentCourse> studentCourses)
+        {
+            StudentId = theStudent.Id;
+            Name = theStudent.Name;
+            TeacherName = theStudent.Teacher.Name;
+
+            CourseText = "";
+            for (int i = 0; i < studentCourses.Count; i++)
+            {
+                CourseText += studentCourses[i].Course.Name;
+                if (i < studentCourses.Count - 1)
+                {
+                    CourseText += ", ";
+                }
+            }
+        }
+    }
+}
